4df29b421019b60c5fc98ec59f261d77677d4db0
Nikita Tkachenko <111963844+gussslp@users.noreply.github.com>
2023-04-30 19:48:38 +0300

    vndjak

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import openai
 
-openai.api_key = "api_key"
-
-text = input("enter text: ")
-
-prompt=f"analyze the content of the text, display statistics on the number of words, sentences and named entities in this text : {text}"
-
+openai.api_key = ""
 model = "text-davinci-003"
-response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=50)
 
-generated_text = response.choices[0].text
-print(generated_text)
+import os
+txt_list = []
+for file in os.listdir():
+    if file.endswith(".txt"):
+        txt_list.append(file)
+
+with open(txt_list[0]) as f:
+    text = f.readlines()
+
+    prompt=f"analyze the content of the text, display statistics on the number of words, sentences and named entities in this text : {text}"
+
+    response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=50)
+
+    generated_text = response.choices[0].text
+    print(generated_text)
